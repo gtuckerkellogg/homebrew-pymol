301750136d6a9f8bdbc989f5fe0c59d3ab14cf5e
mazubieta <mazubieta@gmail.com>
2012-08-06 09:45:50 -0500

    Update pymol.rb
    i believe this works. need to test on fresh machines to be sure.  pymol fails in my virtual machine, but I believe that's because it's a virtual machine. error:
    
    unknown error code: invalid pixel format
    kCGErrorFailure: Set a breakpoint @CGErrorBreakpoint() to catch
    X Error of failed request: GLXBadContext

--- pymol.rb
+++ pymol.rb
@@ -20,9 +20,11 @@ depends_on :x11 # if your formula requires any X11/XQuartz components
     ENV.prepend 'LDFLAGS', '-lGLEW'
 
     system "python setup.py build"
-    system "python setup.py install --prefix=#{HOMEBREW_PREFIX}/bin"
-    system "python setup2.py install --prefix=#{HOMEBREW_PREFIX}/bin"
-    # system "ln -s pymol #{HOMEBREW_PREFIX}/bin/pymol"
+    system "python setup.py install"
+    system "python setup2.py install"
+    system "mkdir -p #{HOMEBREW_CELLAR}/pymol/1.3r2"
+    system "cp -r . #{HOMEBREW_CELLAR}/pymol/1.3r2"
+    system "ln -s #{HOMEBREW_CELLAR}/pymol/1.3r2/pymol #{HOMEBREW_PREFIX}/bin/pymol"
   end
 
   def test
